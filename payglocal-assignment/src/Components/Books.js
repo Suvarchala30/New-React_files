23268ef8bc2ac975c819b2de3eeeba7c52c6464d
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-05-10 17:33:28 +0530

    changes

--- payglocal-assignment/src/Components/Books.js
+++ payglocal-assignment/src/Components/Books.js
@@ -15,20 +15,19 @@ const Books = () => {
   const [showPrice, setShowPrice] = useState(false);
   const [minPrice, setMinPrice] = useState();
   const [maxPrice, setMaxPrice] = useState();
-  const [slider,setSlider]=useState(19);
-  const [showRange,setShowRange]=useState(false)
-  const [rangeDrop,setRangeDrop]=useState(1)
-  const [count,setCount]=useState(0)
-  const [showFilter,setShowFilter]=useState(false)
-  const [inputRange,setInputRange]=useState(19)
+  const [slider, setSlider] = useState(19);
+  const [showRange, setShowRange] = useState(false);
+  const [rangeDrop, setRangeDrop] = useState(1);
+  const [count, setCount] = useState(0);
+  const [showFilter, setShowFilter] = useState(false);
+  const [inputRange, setInputRange] = useState(19);
   const url =
     "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
   useEffect(() => {
     fetchUrl();
-    if(endIndex<dropValue){
-        setNextDisable(true)
+    if (endIndex < dropValue) {
+      setNextDisable(true);
     }
-
   });
   //Fetching URL
   const fetchUrl = async () => {
@@ -36,7 +35,7 @@ const Books = () => {
     const receivedData = await response.json();
     setData(receivedData);
   };
-//Getting search value
+  //Getting search value
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -53,19 +52,19 @@ const Books = () => {
   const handleNext = () => {
     setPrevDisable(false);
     setNextDisable(false);
-    if(endIndex<dropValue){
-        setNextDisable(true)
+    if (endIndex < dropValue) {
+      setNextDisable(true);
     }
     setStartIndex(endIndex);
     setendIndex(Number(endIndex) + Number(dropValue));
-    if (endIndex > 54 || startIndex>54-(2*dropValue) ) {
+    if (endIndex > 54 || startIndex > 54 - 2 * dropValue) {
       setNextDisable(true);
       setPrevDisable(false);
       setStartIndex(53 - dropValue);
       setendIndex(54);
     }
   };
-//previous page button handling
+  //previous page button handling
   const handlePrevious = () => {
     setPrevDisable(false);
     setNextDisable(false);
@@ -83,72 +82,70 @@ const Books = () => {
   const handleFilter = () => {
     setBtnTrigger(!btnTrigger);
 
-  getPrices();
+    getPrices();
   };
   //Price range toggle
   const handlePriceRange = (e) => {
     e.preventDefault();
     setShowPrice(!showPrice);
-    
-
   };
   const PriceRange = (e) => {
-    setInputRange(e.target.value)
-    setSlider(e.target.value)
-    if(count<2){
-        setCount(count+1)
+    setInputRange(e.target.value);
+    setSlider(e.target.value);
+    if (count < 2) {
+      setCount(count + 1);
     }
-    
   };
   //Get min and max prices from API
-  const getPrices=(()=>{
-   let arr=[]
-   for(let i=0;i<data.length;i++){
-arr.push(data[i].price)
-//arr.push(Number(data[i].price)+1000 )
-      
-   }
-   const min=Math.min(...arr)
-   
-   const max=Math.max(...arr)
-   setMinPrice(min)
-   setMaxPrice(max)
-  })
-//Handle apply button on click apply filters
-  const handleApply=(()=>{
-     console.log(slider)
-     console.log(rangeDrop)
-      setBtnTrigger(!btnTrigger);
-      
-  })
-  //toggle ratings
-  const handleShowRange=(()=>{
-      setShowRange(!showRange)
-  })
-  //get ratings value to be filtered
-  const handleRatingRange=((e)=>{
-    setRangeDrop(e.target.value)
-    if(count<2){
-        setCount(count+1)
+  const getPrices = () => {
+    let arr = [];
+    for (let i = 0; i < data.length; i++) {
+      arr.push(data[i].price);
+      //arr.push(Number(data[i].price)+1000 )
     }
-  })
+    const min = Math.min(...arr);
+
+    const max = Math.max(...arr);
+    setMinPrice(min);
+    setMaxPrice(max);
+  };
+  //Handle apply button on click apply filters
+  const handleApply = () => {
+    console.log(slider);
+    console.log(rangeDrop);
+    setBtnTrigger(!btnTrigger);
+  };
+  //toggle ratings
+  const handleShowRange = () => {
+    setShowRange(!showRange);
+  };
+  //get ratings value to be filtered
+  const handleRatingRange = (e) => {
+    setRangeDrop(e.target.value);
+    if (count < 2) {
+      setCount(count + 1);
+    }
+  };
   //hover filter
-  const handleHover=(()=>{
-    setShowFilter(!showFilter)
-  })
-  const handleLeave=(()=>{
-      setShowFilter(!showFilter)
-  })
-  
+  const handleHover = () => {
+    setShowFilter(!showFilter);
+  };
+  const handleLeave = () => {
+    setShowFilter(!showFilter);
+  };
+
   return (
     <div className="Books">
       <div className="header">
-          <div className="filterArea">
-         
-        <button className="filterBtn" onClick={handleFilter} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
-          ({count}) Filters
-        </button>
-        
+        <div className="filterArea">
+          <button
+            className="filterBtn"
+            onClick={handleFilter}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
+          >
+            ({count}) Filters
+          </button>
         </div>
         <form onSubmit={handleClick}>
           <input
@@ -160,33 +157,34 @@ arr.push(data[i].price)
           <FaSearch className="searchIcon" />
         </form>
       </div>
-      {
-              showFilter?<div className="filterApplied">
-                  <p className="priceFilter">Max Price: {slider}</p>
-                  <p className="ranteFilter">Rating: {rangeDrop} and above</p>
-              </div>:""
-          }
+      {showFilter ? (
+        <div className="filterApplied">
+          <p className="priceFilter">Max Price: {slider}</p>
+          <p className="ranteFilter">Rating: {rangeDrop} and above</p>
+        </div>
+      ) : (
+        ""
+      )}
       <div className="container">
-        {data.filter((val)=>{
-            if(val.rating>=rangeDrop){
-                return val
+        {data
+          .filter((val) => {
+            if (val.rating >= rangeDrop) {
+              return val;
+            } else {
+              return null;
             }
-            else{
-                return null
-            }
+          })
 
-            
-        })
-        
-        .filter(val => { return val.price < parseInt(slider,10)})
-        
+          .filter((val) => {
+            return val.price < parseInt(slider, 10);
+          })
+
           .filter((val) => {
             if (search === "") {
               return val;
             } else if (val.name.toLowerCase().includes(search.toLowerCase())) {
               return val;
             }
-            
           })
           .slice(startIndex, endIndex)
           .map((item, key) => {
@@ -212,7 +210,7 @@ arr.push(data[i].price)
                   </p>
                   <p className="itemPrice">
                     <span className="title">INR: </span>
-                    {/* {Math.floor(Number(item.price)+1000)} */}
+
                     {item.price}
                   </p>
                   <p className="itemType">
@@ -267,33 +265,46 @@ arr.push(data[i].price)
               Price Range
             </button>
             {showPrice ? (
-                <div className="rangeSection">
-                    <div>
-              <input type="range" min={minPrice} max={maxPrice} onChange={PriceRange} value={inputRange}/>
-              </div>
-              <div className="priceDisplay">
-              <span className="minDis">Min: {minPrice}</span>
-              <span className="maxDis">Max: {maxPrice}</span>
-              </div>
+              <div className="rangeSection">
+                <div>
+                  <input
+                    type="range"
+                    min={minPrice}
+                    max={maxPrice}
+                    onChange={PriceRange}
+                    value={inputRange}
+                  />
+                </div>
+                <div className="priceDisplay">
+                  <span className="minDis">Min: {minPrice}</span>
+                  <span className="maxDis">Max: {maxPrice}</span>
+                </div>
               </div>
             ) : null}
-            <button className="ratingRange" onClick={handleShowRange}>Rating Range</button>
-            {showRange?(
-                <div className="ratingSection">
-                    <label htmlFor="Rating">Show Rating Above: </label>
-                    <select className="ratingRange" value={rangeDrop} name="Rating" onChange={handleRatingRange}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
-                </div>
-            )
-        :null
-        }
+            <button className="ratingRange" onClick={handleShowRange}>
+              Rating Range
+            </button>
+            {showRange ? (
+              <div className="ratingSection">
+                <label htmlFor="Rating">Show Rating Above: </label>
+                <select
+                  className="ratingRange"
+                  value={rangeDrop}
+                  name="Rating"
+                  onChange={handleRatingRange}
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </div>
+            ) : null}
           </div>
-          <button className="applyBtn" onClick={handleApply}>Apply</button>
+          <button className="applyBtn" onClick={handleApply}>
+            Apply
+          </button>
         </div>
       </Popup>
     </div>
